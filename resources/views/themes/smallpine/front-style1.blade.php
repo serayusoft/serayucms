b85a1bece9653719e421b1dd1276e7727185d544
serayusoft <serayusoft@gmail.com>
2016-10-17 01:00:23 +0700

    Add image sample 	new file:   public/uploads/sample-image-1.jpg 	new file:   public/uploads/sample-image-2.jpg 	new file:   public/uploads/sample-image-3.jpg 	new file:   public/uploads/sample-image-4.jpg 	new file:   public/uploads/sample-image-5.jpg 	new file:   public/uploads/sample-image-6.jpg 	new file:   public/uploads/sample-image-7.jpg

---
+++ resources/views/themes/smallpine/front-style1.blade.php
@@ -0,0 +1,12 @@
+{{ Widget::group('post_slider') }}
+
+<div id="primary" class="content-area col-md-9">
+	<main id="main" class="site-main" role="main">
+		@yield('content')
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+
+<aside id="sidebar" class="col-md-3" role="complementary">
+	{{ Widget::group('sidebar') }}
+</aside>
